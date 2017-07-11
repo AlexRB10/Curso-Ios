a5d32bc1754bb623c6535eed93bddf6bb5cbba07
Alumno <alumno@Mac-mini-de-ulpgc-3.local>
2017-07-11 18:08:36 +0100

    InsertarDatos

--- Proyecto Alex/Singleton.swift
+++ Proyecto Alex/Singleton.swift
@@ -13,9 +13,24 @@ class ControllerData {
     static let shareController = ControllerData()
     private init(){
         
+        
     }
     
-    var album : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+    var albumes : [Album] = [Album.init(nombreAlbum: "El Dorado", nombreArtista: "Shakira", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "Munay", nombreArtista: "Vanesa Martín", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "Quítate Las Gafas", nombreArtista: "Melendi", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "La Montaña Rusa", nombreArtista: "Dani Martín", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "Qué Tú Tienes", nombreArtista: "Dvicio", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "El Planeta Imaginario", nombreArtista: "La Oreja de Van Gogh", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "Hijos Del Mar", nombreArtista: "Davis Bisbal", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "Primera Cita", nombreArtista: "CNCO", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "Destino", nombreArtista: "Antonio Orozco", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "Divide", nombreArtista: "Ed Sheeran", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "24K Magic", nombreArtista: "Bruno Mars", fotoAlbum: ""),
+                             Album.init(nombreAlbum: "Dangerous Woman", nombreArtista: "Ariana Grande", fotoAlbum: "")]
+    
+    
+    var elDorado : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
@@ -25,7 +40,7 @@ class ControllerData {
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),]
     
-    var album1 : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+    var munay : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
@@ -35,7 +50,7 @@ class ControllerData {
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),]
     
-    var album2 : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+    var quitateLasGafas : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
@@ -45,7 +60,7 @@ class ControllerData {
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),]
     
-    var album3 : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+    var laMontañaRusa : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
@@ -55,7 +70,7 @@ class ControllerData {
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),]
     
-    var album4 : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+    var queTuTienes : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
@@ -65,7 +80,7 @@ class ControllerData {
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),]
     
-    var album5 : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+    var elPlatenaImagiario : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
@@ -74,6 +89,67 @@ class ControllerData {
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),
                            Pista.init(nombrePista: "", nombreFoto: ""),]
+    
+    var hijosDelMar : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+                                    Pista.init(nombrePista: "", nombreFoto: ""),
+                                    Pista.init(nombrePista: "", nombreFoto: ""),
+                                    Pista.init(nombrePista: "", nombreFoto: ""),
+                                    Pista.init(nombrePista: "", nombreFoto: ""),
+                                    Pista.init(nombrePista: "", nombreFoto: ""),
+                                    Pista.init(nombrePista: "", nombreFoto: ""),
+                                    Pista.init(nombrePista: "", nombreFoto: ""),
+                                    Pista.init(nombrePista: "", nombreFoto: ""),]
+    
+    var primeraCita : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),]
+    
+    var destino : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+                                        Pista.init(nombrePista: "", nombreFoto: ""),
+                                        Pista.init(nombrePista: "", nombreFoto: ""),
+                                        Pista.init(nombrePista: "", nombreFoto: ""),
+                                        Pista.init(nombrePista: "", nombreFoto: ""),
+                                        Pista.init(nombrePista: "", nombreFoto: ""),
+                                        Pista.init(nombrePista: "", nombreFoto: ""),
+                                        Pista.init(nombrePista: "", nombreFoto: ""),
+                                        Pista.init(nombrePista: "", nombreFoto: ""),]
+    
+    var divide : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),]
+    
+    var magic : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),
+                                 Pista.init(nombrePista: "", nombreFoto: ""),]
+    
+    var dangerousWoman : [Pista] = [Pista.init(nombrePista: "", nombreFoto: ""),
+                             Pista.init(nombrePista: "", nombreFoto: ""),
+                             Pista.init(nombrePista: "", nombreFoto: ""),
+                             Pista.init(nombrePista: "", nombreFoto: ""),
+                             Pista.init(nombrePista: "", nombreFoto: ""),
+                             Pista.init(nombrePista: "", nombreFoto: ""),
+                             Pista.init(nombrePista: "", nombreFoto: ""),
+                             Pista.init(nombrePista: "", nombreFoto: ""),
+                             Pista.init(nombrePista: "", nombreFoto: ""),]
+    
 }
 
 //ControllerData.shareController.//
