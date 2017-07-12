df968adb90c01c3e768d62973ff411701880006a
Alumno <alumno@Mac-mini-de-ulpgc-3.local>
2017-07-12 18:55:47 +0100

    Reproducotr

--- Proyecto Alex/ControladorReproductor.swift
+++ Proyecto Alex/ControladorReproductor.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2017 Alex. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import AVKit
+import AVFoundation
+
+class ControladorReproductor :UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let videoURL = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+        let player = AVPlayer(url: videoURL!)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(playerLayer)
+        player.play()
+    }
+}
