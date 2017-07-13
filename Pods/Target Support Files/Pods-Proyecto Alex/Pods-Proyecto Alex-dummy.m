e58917a8d18af58391b3c152155137f4d23310a6
Alumno <alumno@Mac-mini-de-ulpgc-3.local>
2017-07-13 10:09:17 +0100

    Update pods

--- Pods/Target Support Files/Pods-Proyecto Alex/Pods-Proyecto Alex-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Proyecto_Alex : NSObject
-@end
-@implementation PodsDummy_Pods_Proyecto_Alex
-@end
--- Pods/Target Support Files/Pods-Proyecto AlexUITests/Pods-Proyecto AlexUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Proyecto_AlexUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Proyecto_AlexUITests
-@end
